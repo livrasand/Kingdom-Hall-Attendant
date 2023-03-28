d1ff02a51b57d46e8e59d9d0fc3e7b8d03b26262
Ramón Menor <ramonmenor@gmail.com>
2023-03-28 21:36:41 +0200

    Sidebar: muestra el valor pulsado

--- kha-electron/src/lib/SideBar.svelte
+++ kha-electron/src/lib/SideBar.svelte
@@ -1,6 +1,6 @@
 
 <script>
-
+import { page } from '$app/stores';
 </script>
 
 <nav class="menu" aria-label="Person settings">
@@ -23,111 +23,111 @@
           </div>
        </div>
     </div>
-    <a id="link-congregacion" class="menu-item" href="/dashboard/congregacion">
+    <a class="menu-item" href="/dashboard/congregacion" aria-current={$page.url.pathname === '/dashboard/congregacion' ? 'page' : undefined}>
     <i class="jwIcon e51"></i>
     Congregación
     </a>
-    <a id="link-vidayministerio" class="menu-item" href="/dashboard/vidayministerio">
+    <a class="menu-item" href="/dashboard/vidayministerio" aria-current={$page.url.pathname === '/dashboard/vidayministerio' ? 'page' : undefined}>
     <i class="jwIcon e118"></i>
     Vida y Ministerio
     </a>
-    <a id="link-estudioatalaya" class="menu-item" href="/dashboard/estudioatalaya">
+    <a class="menu-item" href="/dashboard/estudioatalaya" aria-current={$page.url.pathname === '/dashboard/estudioatalaya' ? 'page' : undefined}>
     <i class="jwIcon e95"></i>
     Estudio de la Atalaya
     </a>
-    <a id="link-consejooratoria" class="menu-item" href="/dashboard/consejooratoria">
+    <a class="menu-item" href="/dashboard/consejooratoria" aria-current={$page.url.pathname === '/dashboard/consejooratoria' ? 'page' : undefined}>
     <i class="jwIcon e185"></i>
     Consejo de la Oratoria
     </a>
-    <a class="menu-item" href="/dashboard/esquemapresidente">
+    <a class="menu-item" href="/dashboard/esquemapresidente" aria-current={$page.url.pathname === '/dashboard/esquemapresidente' ? 'page' : undefined}>
     <i class="jwIcon e23"></i>
     Esquema del Presidente
     </a>
-    <a class="menu-item" href="/dashboard/soportetecnico">
+    <a class="menu-item" href="/dashboard/soportetecnico" aria-current={$page.url.pathname === '/dashboard/soportetecnico' ? 'page' : undefined}>
     <i class="jwIcon e35"></i>
     Soporte tecnico
     </a>
-    <a class="menu-item" href="/dashboard/orgreuniones">
+    <a class="menu-item" href="/dashboard/orgreuniones" aria-current={$page.url.pathname === '/dashboard/orgreuniones' ? 'page' : undefined}>
     <i class="jwIcon e186"></i>
     Organización de las reuniones
     </a>
-    <a class="menu-item" href="/dashboard/oradorespublicos">
+    <a class="menu-item" href="/dashboard/oradorespublicos" aria-current={$page.url.pathname === '/dashboard/oradorespublicos' ? 'page' : undefined}>
     <i class="jwIcon e175"></i>
     Oradores públicos
     </a>
-    <a class="menu-item" href="/dashboard/hospitalidades">
+    <a class="menu-item" href="/dashboard/hospitalidades" aria-current={$page.url.pathname === '/dashboard/hospitalidades' ? 'page' : undefined}>
     <i class="jwIcon e53"></i>
     Hospitalidades
     </a>
-    <a class="menu-item" href="/dashboard/bosquejos">
+    <a class="menu-item" href="/dashboard/bosquejos" aria-current={$page.url.pathname === '/dashboard/bosquejos' ? 'page' : undefined}>
     <i class="jwIcon siete"></i>
     Bosquejos
     </a>
-    <a class="menu-item" href="/dashboard/limpieza">
+    <a class="menu-item" href="/dashboard/limpieza" aria-current={$page.url.pathname === '/dashboard/limpieza' ? 'page' : undefined}>
     <i class="jwIcon e111"></i>
     Limpieza y Matenimiento
     </a>  
-    <a class="menu-item" href="/dashboard/territorios">
+    <a class="menu-item" href="/dashboard/territorios" aria-current={$page.url.pathname === '/dashboard/territorios' ? 'page' : undefined}>
     <i class="jwIcon e33"></i>
     Territorios
     </a>
-    <a class="menu-item" href="/dashboard/visitassc">
+    <a class="menu-item" href="/dashboard/visitassc" aria-current={$page.url.pathname === '/dashboard/visitassc' ? 'page' : undefined}>
     <i class="jwIcon e87"></i>
     Visitas del Superintendente de Circuito 
     </a>
-    <a class="menu-item" href="/dashboard/cuidadocj">
+    <a class="menu-item" href="/dashboard/cuidadocj" aria-current={$page.url.pathname === '/dashboard/cuidadocj' ? 'page' : undefined}>
     <i class="jwIcon e17"></i>
     Cuidado de cesped y jardin
     </a>
-    <a class="menu-item" href="/dashboard/mantenimientoldc">
+    <a class="menu-item" href="/dashboard/mantenimientoldc" aria-current={$page.url.pathname === '/dashboard/mantenimientoldc' ? 'page' : undefined}>
     <i class="jwIcon e172"></i>
     Matenimiento de LDC
     </a>
-    <a class="menu-item" href="/dashboard/grupopredicacion">
+    <a class="menu-item" href="/dashboard/grupopredicacion" aria-current={$page.url.pathname === '/dashboard/grupopredicacion' ? 'page' : undefined}>
     <i class="jwIcon e114"></i>
     Grupos de predicación
     </a>
-    <a class="menu-item" href="/dashboard/visitaspastoreo">
+    <a class="menu-item" href="/dashboard/visitaspastoreo" aria-current={$page.url.pathname === '/dashboard/visitaspastoreo' ? 'page' : undefined}>
     <i class="jwIcon e106"></i>
     Visitas de pastoreo
     </a>
-    <a class="menu-item" href="/dashboard/exhibidores">
+    <a class="menu-item" href="/dashboard/exhibidores" aria-current={$page.url.pathname === '/dashboard/exhibidores' ? 'page' : undefined}>
     <i class="jwIcon e140"></i>
     Mesas y exhibidores portatiles
     </a>
-    <a class="menu-item" href="/dashboard/serviciocampo">
+    <a class="menu-item" href="/dashboard/serviciocampo" aria-current={$page.url.pathname === '/dashboard/serviciocampo' ? 'page' : undefined}>
     <i class="jwIcon e56"></i>
     Servicio del campo
     </a>
-    <a class="menu-item" href="/dashboard/informes">
+    <a class="menu-item" href="/dashboard/informes" aria-current={$page.url.pathname === '/dashboard/informes' ? 'page' : undefined}>
     <i class="jwIcon e184"></i>
     Informes
     </a>
-    <a class="menu-item" href="/dashboard/contabilidad">
+    <a class="menu-item" href="/dashboard/contabilidad" aria-current={$page.url.pathname === '/dashboard/contabilidad' ? 'page' : undefined}>
     <i class="jwIcon e24"></i>
     Contabilidad
     </a>
-    <a class="menu-item" href="/dashboard/asistenciareu">
+    <a class="menu-item" href="/dashboard/asistenciareu" aria-current={$page.url.pathname === '/dashboard/asistenciareu' ? 'page' : undefined}>
     <i class="jwIcon e173"></i>
     Asistencia a las reuniones
     </a>
-    <a class="menu-item" href="/dashboard/literrevistas">
+    <a class="menu-item" href="/dashboard/literrevistas" aria-current={$page.url.pathname === '/dashboard/literrevistas' ? 'page' : undefined}>
     <i class="jwIcon e40"></i>
     Literatura y revistas
     </a>
-    <a class="menu-item" href="/dashboard/asuntosjudiciales">
+    <a class="menu-item" href="/dashboard/asuntosjudiciales" aria-current={$page.url.pathname === '/dashboard/asuntosjudiciales' ? 'page' : undefined}>
     <i class="jwIcon e46"></i>
     Asuntos judiciales
     </a>
-    <a class="menu-item" href="/dashboard/estadisticas">
+    <a class="menu-item" href="/dashboard/estadisticas" aria-current={$page.url.pathname === '/dashboard/estadisticas' ? 'page' : undefined}>
     <i class="jwIcon e181"></i>
     Estadisticas de la congregacion
     </a>
-    <a class="menu-item" href="/dashboard/copias">
+    <a class="menu-item" href="/dashboard/copias" aria-current={$page.url.pathname === '/dashboard/copias' ? 'page' : undefined}>
     <i class="jwIcon e120"></i>
     Copias de seguridad
     </a>
-    <a class="menu-item" href="/dashboard/configuracion">
+    <a class="menu-item" href="/dashboard/configuracion" aria-current={$page.url.pathname === '/dashboard/configuracion' ? 'page' : undefined}>
     <i class="jwIcon e77"></i>
     Configuracion
     </a>
